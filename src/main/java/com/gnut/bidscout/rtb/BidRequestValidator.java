6a72212622d1f3f9388c969e5462a6eb17481a40
Rick Shea <rick@beachfront.com>
2018-11-21 13:29:16 -0500

    WIP

--- src/main/java/com/gnut/bidscout/rtb/BidRequestValidator.java
+++ src/main/java/com/gnut/bidscout/rtb/BidRequestValidator.java
@@ -14,6 +14,7 @@ public class BidRequestValidator {
     public boolean validateBidRequest(BidRequest bidRequest, AuctionRecord record) {
         if(bidRequest.getImp() == null) {
             record.getBidRequestErrors().add(BidRequestError.NO_IMPRESSION.value());
+            return false;
         }
 
         return true;
