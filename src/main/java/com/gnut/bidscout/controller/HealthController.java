10d394e25793ec28d9318fbbef827a010f5a0bba
Rick Shea <rick@beachfront.com>
2018-11-24 10:20:54 -0500

    HC

---
+++ src/main/java/com/gnut/bidscout/controller/HealthController.java
@@ -0,0 +1,20 @@
+package com.gnut.bidscout.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping(value = "/health")
+public class HealthController {
+
+    @RequestMapping(value="/server", method = RequestMethod.GET, produces = "application/json")
+    public void handleBidRequestPostWithKey(
+            HttpServletResponse response
+    ) {
+        response.setStatus(204);
+    }
+}
