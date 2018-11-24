7043649403d5cd33cdbba67136b5932e29af311a
Rick Shea <rick@beachfront.com>
2018-11-24 15:04:14 -0500

    WIP

--- src/main/java/com/gnut/bidscout/html/AdMarkup.java
+++ src/main/java/com/gnut/bidscout/html/AdMarkup.java
@@ -9,6 +9,9 @@ import java.math.BigDecimal;
 
 @Component
 public class AdMarkup {
+    private static final String SYNC_URL = "http://app.auctionscout.net/sync/user";
+    private static final String IMP_URL ="http://app.auctionscout.net/imp";
+    private static final String CLICK_URL = "http://app.auctionscout.net/click";
     private static final String WIDTH_MACRO = "WIDTH";
     private static final String HEIGHT_MACRO = "HEIGHT";
     private static final String IMPRESSION_MACRO = "IMPRESSION";
@@ -16,7 +19,7 @@ public class AdMarkup {
     private static final String CLICK_MACRO = "CLICK";
     private static final String ASSET_MACRO = "ASSET";
     private static final String HTML_OPEN = "<div style=\"width:" + WIDTH_MACRO + "px; height:" + HEIGHT_MACRO + "px;\">";
-    private static final String HTML_OPEN_CREATIVE_FOR_ALL_SIZES = "<div style=\"border: 1px solid black; background: #CCC; width:" + WIDTH_MACRO + "px; height:" + HEIGHT_MACRO + "px;\">";
+    private static final String CREATIVE_FOR_ALL_SIZES = "<div style=\"border: 1px solid black; background: #CCC; width:" + WIDTH_MACRO + "px; height:" + HEIGHT_MACRO + "px;\">";
     private static final String HTML_CLOSE = "</div>";
     private static final String CLICK_OPEN = "<a href=\"" + CLICK_MACRO + "\" target=\"_blank\">";
     private static final String CLICK_CLOSE = "</a>";
@@ -25,32 +28,99 @@ public class AdMarkup {
     private static final String ASSET_IMG = "<img src=\"" + ASSET_MACRO + "\" style=\"width:" + WIDTH_MACRO + "px; height:" + HEIGHT_MACRO + "px;\">";
 
     public String generateDisplayMarkup(BigDecimal price, String bidRequestId, Campaign campaign, Creative creative) {
+        if (!Strings.isNullOrEmpty(creative.getAdm())) {
+            return buildCustomAdmCreative(price, bidRequestId, campaign, creative);
+        } else if (creative.getW() == 0 && creative.getH() == 0) {
+            return buildDefaultCreative(price, bidRequestId, campaign, creative);
+        } else {
+            return buildCreativeWithCustomAsset(price, bidRequestId, campaign, creative);
+        }
+    }
+
+    private String buildCustomAdmCreative(BigDecimal price, String bidRequestId, Campaign campaign, Creative creative) {
         final StringBuilder adm = new StringBuilder();
 
-        if (Strings.isNullOrEmpty(creative.getAdm())) {
-            adm.append(HTML_OPEN)
-                    .append(CLICK_OPEN)
-                    .append(ASSET_IMG)
-                    .append(CLICK_CLOSE);
-        } else if (creative.getW() == 0 && creative.getH() == 0) {
-            adm.append(HTML_OPEN_CREATIVE_FOR_ALL_SIZES);
-        } else {
-            adm.append(HTML_OPEN)
-                    .append(creative.getAdm());
-        }
-
-        adm.append(IMPRESSION_IMG_HIDDEN);
+        adm.append(HTML_OPEN)
+                .append(creative.getAdm())
+                .append(IMPRESSION_IMG_HIDDEN);
 
         String sync = "";
         if (campaign.isSyncUsers() || creative.isSyncUsers()) {
             adm.append(SYNC_IMG_HIDDEN);
-            sync = "http://localhost:8080/sync/user";
+            sync = SYNC_URL;
         }
 
         adm.append(HTML_CLOSE);
 
+        String imp = buildImpressionPixel(campaign, creative, bidRequestId, price);
+
+        return adm.toString()
+                .replace(IMPRESSION_MACRO, imp)
+                .replace(SYNC_MACRO, sync)
+                .replaceAll(WIDTH_MACRO, String.valueOf(creative.getW()))
+                .replaceAll(HEIGHT_MACRO, String.valueOf(creative.getH()));
+    }
+
+    private String buildDefaultCreative(BigDecimal price, String bidRequestId, Campaign campaign, Creative creative) {
+        final StringBuilder adm = new StringBuilder();
+
+        adm.append(HTML_OPEN)
+                .append(CLICK_OPEN)
+                .append(CREATIVE_FOR_ALL_SIZES)
+                .append(CLICK_CLOSE)
+                .append(IMPRESSION_IMG_HIDDEN);
+
+        String sync = "";
+        if (campaign.isSyncUsers() || creative.isSyncUsers()) {
+            adm.append(SYNC_IMG_HIDDEN);
+            sync = SYNC_URL;
+        }
+
+        adm.append(HTML_CLOSE);
+
+        String imp = buildImpressionPixel(campaign, creative, bidRequestId, price);
+        String click = buildClickPixel(campaign, creative, bidRequestId);
+
+        return adm.toString()
+                .replace(IMPRESSION_MACRO, imp)
+                .replace(SYNC_MACRO, sync)
+                .replace(CLICK_MACRO, click)
+                .replaceAll(WIDTH_MACRO, String.valueOf(creative.getW()))
+                .replaceAll(HEIGHT_MACRO, String.valueOf(creative.getH()));
+    }
+
+    private String buildCreativeWithCustomAsset(BigDecimal price, String bidRequestId, Campaign campaign, Creative creative) {
+        final StringBuilder adm = new StringBuilder();
+
+        adm.append(HTML_OPEN)
+                .append(CLICK_OPEN)
+                .append(ASSET_IMG)
+                .append(CLICK_CLOSE)
+                .append(IMPRESSION_IMG_HIDDEN);
+
+        String sync = "";
+        if (campaign.isSyncUsers() || creative.isSyncUsers()) {
+            adm.append(SYNC_IMG_HIDDEN);
+            sync = SYNC_URL;
+        }
+
+        adm.append(HTML_CLOSE);
+
+        String imp = buildImpressionPixel(campaign, creative, bidRequestId, price);
+        String click = buildClickPixel(campaign, creative, bidRequestId);
+
+        return adm.toString()
+                .replace(IMPRESSION_MACRO, imp)
+                .replace(SYNC_MACRO, sync)
+                .replace(CLICK_MACRO, click)
+                .replace(ASSET_MACRO, creative.getCreativeUrl())
+                .replaceAll(WIDTH_MACRO, String.valueOf(creative.getW()))
+                .replaceAll(HEIGHT_MACRO, String.valueOf(creative.getH()));
+    }
+
+    private String buildImpressionPixel(Campaign campaign, Creative creative, String bidRequestId, BigDecimal price) {
         StringBuilder imp = new StringBuilder();
-        imp.append("http://localhost:8080/imp")
+        return imp.append(IMP_URL)
                 .append("/")
                 .append(campaign.getOwner())
                 .append("/")
@@ -67,9 +137,11 @@ public class AdMarkup {
                 .append("?cb=")
                 .append(System.currentTimeMillis())
                 .toString();
+    }
 
+    private String buildClickPixel(Campaign campaign, Creative creative, String bidRequestId) {
         StringBuilder click = new StringBuilder();
-        click.append("http://localhost:8080/click")
+        return click.append(CLICK_URL)
                 .append("/")
                 .append(campaign.getOwner())
                 .append("/")
@@ -81,13 +153,5 @@ public class AdMarkup {
                 .append("?cb=")
                 .append(System.currentTimeMillis())
                 .toString();
-
-        return adm.toString()
-                .replace(IMPRESSION_MACRO, imp)
-                .replace(SYNC_MACRO, sync)
-                .replace(CLICK_MACRO, click)
-                .replace(ASSET_MACRO, creative.getCreativeUrl())
-                .replaceAll(WIDTH_MACRO, String.valueOf(creative.getW()))
-                .replaceAll(HEIGHT_MACRO, String.valueOf(creative.getH()));
     }
 }
